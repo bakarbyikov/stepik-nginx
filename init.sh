76e36112fb78a4d7ca463a578c0ac14bf1a84319
Andrey <Bakarbyikov@gmail.com>
2021-12-01 06:51:03 +0500

    no more ~

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp -r ./web ~/web
 
-sudo ln -f -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -f -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
