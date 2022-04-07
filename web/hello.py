79c82b02a467213d3f0960af928012b3a3ce7758
Andrey <Bakarbyikov@gmail.com>
2022-04-07 10:35:41 +0500

    fixed app

--- web/hello.py
+++ web/hello.py
@@ -8,13 +8,8 @@ def app(environ, start_response):
         ('Content-Type', 'text/plain') 
     ]
     
-#    body = environ['QUERY_STRING'].split('&')
-#    body = map(str.encode, body)
-    body = environ['QUERY_STRING'].split('&')
-    body = [(s+'\n').encode() for s in body]
+    body = environ['QUERY_STRING']
+    body = body.replace('&', '\n')
+    body = body.encode()
     start_response(status, headers) 
-    return body
-
-#env = {'QUERY_STRING': '/?a=1&a=2&b=3'}
-#f = lambda x: x
-#wsgi_application(f)
+    return [body]
