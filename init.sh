c855ca5d7e50e2a7bfb9cc7f9dcb12e7dd6ffd27
Andrey <Bakarbyikov@gmail.com>
2022-04-07 10:38:39 +0500

    added command to delete default nginx config

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 sudo rm -r /home/box/web
 sudo cp -r ./web /home/box/web
 
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -f -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
