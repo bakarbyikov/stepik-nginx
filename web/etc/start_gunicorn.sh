98e8597a00f452163bb115062fbbb60d1652ef26
Oleg Oleg <bakarbyikov@gmail.com>
2022-04-07 16:33:39 +0500

    django ask project added

--- web/etc/start_gunicorn.sh
+++ web/etc/start_gunicorn.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gunicorn -c /home/box/web/etc/gunicorn_config.py --chdir /home/box/web hello:app
+gunicorn -c /home/box/web/etc/gunicorn_config.py --chdir /home/box/web hello:app --daemon
+gunicorn -c /home/box/web/etc/gunicorn_config_django.py --chdir /home/box/web/ask ask.wsgi --daemon
