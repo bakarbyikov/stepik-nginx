5772f7d85519e60e95b9c3ee211c06e022c29c3a
Oleg Oleg <bakarbyikov@gmail.com>
2022-04-08 10:18:00 +0500

    log added to gunicorn

--- web/etc/gunicorn_config.py
+++ web/etc/gunicorn_config.py
@@ -1,4 +1,8 @@
 bind = '0.0.0.0:8080'
 workers = 3
-# limit_request_fields = 32000
-# limit_request_field_size = 0
+
+accesslog = "/var/log/gunicorn.access.log"
+errorlog = "/var/log/gunicorn.error.log"
+# Whether to send Django output to the error log 
+capture_output = True
+loglevel = "info"
--- web/etc/gunicorn_config_django.py
+++ web/etc/gunicorn_config_django.py
@@ -1,2 +1,5 @@
+import imp
+
+
+from .gunicorn_config import *
 bind = '0.0.0.0:8000'
-workers = 3
