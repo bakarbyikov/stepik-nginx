06c6e21e038faee8851eda9c87e25a3d11d7bb9a
Oleg Oleg <bakarbyikov@gmail.com>
2022-04-09 14:28:36 +0500

    question manager added

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):                                          
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
 class Question(models.Model):
+    objects = QuestionManager() 
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField()
     rating = models.IntegerField()
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='question_author')
     likes = models.ManyToManyField(User, blank=True, related_name='question_like')
+
 
 class Answer(models.Model):
     text = models.TextField()
