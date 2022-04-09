fedbc3b09b6bc7167059f99c7f63b3ac1cd95e6b
Oleg Oleg <bakarbyikov@gmail.com>
2022-04-09 13:34:27 +0500

    database startup added

--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#files unpackaging
 rm -r /home/box/web
 cp -r ./web /home/box/web
 
+#database startup
+/etc/init.d/mysql start
+mysql -uroot -e "create database database_ask"
+
+#nginx startup
 rm /etc/nginx/sites-enabled/default
 ln -f -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
 
+#gunicorn startup
 sh /home/box/web/etc/start_gunicorn.sh
