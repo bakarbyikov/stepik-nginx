c634dfdc1dbe645c4ecb0ea72e5216257ca1a670
Oleg Oleg <bakarbyikov@gmail.com>
2022-04-09 13:28:13 +0500

    mysql support added

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,17 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'database_ask',  
+        'USER': 'root',  
+        'PASSWORD': '123',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    },
 }
 
 
